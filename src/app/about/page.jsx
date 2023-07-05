2d5f255f75d79189ca0d6fe40db488723728b151
Duy Anh <ndanhdev@gmail.com>
2023-07-05 15:50:05 +0700

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function page() {
+    return (
+        <div className="max-w-6xl mx-auto space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600">About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis maiores eligendi ipsam voluptates distinctio iure
+                itaque sint ullam omnis laboriosam maxime consequuntur, laborum
+                fuga, sunt officiis obcaecati iste optio nulla! Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Reiciendis maiores
+                eligendi ipsam voluptates distinctio iure itaque sint ullam
+                omnis laboriosam maxime consequuntur, laborum fuga, sunt
+                officiis obcaecati iste optio nulla! Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Reiciendis maiores eligendi ipsam
+                voluptates distinctio iure itaque sint ullam omnis laboriosam
+                maxime consequuntur, laborum fuga, sunt officiis obcaecati iste
+                optio nulla!
+            </p>
+        </div>
+    );
+}
